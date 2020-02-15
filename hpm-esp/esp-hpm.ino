07d936076ff001e5f43966bb5eb394a069b3a014
Bertrik Sikken <bertrik@sikken.nl>
2020-02-15 23:47:53 +0100

    Initial code.

---
+++ hpm-esp/esp-hpm.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#include "PubSubClient.h"
+#include "ESP8266WiFi.h"
+#include "WiFiManager.h"
+
+void setup(void)
+{
+}
+
+void loop(void)
+{
+}
+
+
---
+++ hpm-esp/hpma115s.cpp
@@ -0,0 +1,4 @@
+
+#include "hpma115s.h"
+
+
